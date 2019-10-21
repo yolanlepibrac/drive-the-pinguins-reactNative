50863ffccb6455f75d318bd1670228484b75ec8e
yolanlepibrac <yolan.pibrac@gmail.comleavekjcjknvsdvgit config --global user.email yolan.pibrac@gmail.com>
2019-10-22 00:21:49 +0200

    before eject

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import Store from './redux/store/index'
+
+import MyAppComponent from './components/MyAppComponent'
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Provider store={Store}>
+        <MyAppComponent/>
+      </Provider>
     </View>
   );
 }
